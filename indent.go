010ab9892d77a799880b77766bfa91b7960692ba
DazFather <The6daz6@Gmail.com>
2025-10-18 04:07:48 +0200

    Add support for comments

--- indent.go
+++ indent.go
@@ -44,6 +44,36 @@ func (i *indenter) text(ch rune, at int) (indentFn, error) {
 	return i.text, nil
 }
 
+func (i *indenter) slash(ch rune, at int) (indentFn, error) {
+	switch ch {
+	case '/':
+		return i.comment, nil
+	case '*':
+		return i.multilineComment, nil
+	}
+	return i.char(ch, at)
+}
+
+func (i *indenter) comment(ch rune, at int) (indentFn, error) {
+	if at == 0 {
+		return i.char(ch, at)
+	}
+	return i.comment, nil
+}
+
+func (i *indenter) multilineComment(ch rune, at int) (indentFn, error) {
+	if ch != '*' {
+		return i.multilineComment, nil
+	}
+
+	return func(nextCh rune, nextAt int) (indentFn, error) {
+		if nextCh == '/' && nextAt == at+1 {
+			return i.char, nil
+		}
+		return i.multilineComment, nil
+	}, nil
+}
+
 func (i *indenter) char(ch rune, at int) (indentFn, error) {
 	switch ch {
 	case i.toclose:
@@ -66,6 +96,10 @@ func (i *indenter) char(ch rune, at int) (indentFn, error) {
 		return func(ch rune, at int) (indentFn, error) {
 			return i.char, nil
 		}, nil
+	case '/':
+		return i.slash, nil
+	case '#':
+		return i.comment, nil
 	case '(':
 		i.toclose = ch + 1
 		i.brackets.push(i.toclose)
